bb319e638b55cc9af4cba3deecc4617d47c7dfaa
MarioARC21 <marioacr21jk@gmail.com>
2024-02-14 18:48:44 -0600

    Dia 5

---
+++ 30 Days of Code/Day5.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Day5{
+
+    public static void main(String[] args) {
+        
+            Scanner scan = new Scanner(System.in);
+
+            int a = scan.nextInt();
+
+            for (int i = 1; i <= 10; i++) {
+            int b = a * i;
+            System.out.println(a + " x " + i + " = " + b);
+            
+        }
+    }
+
+}
